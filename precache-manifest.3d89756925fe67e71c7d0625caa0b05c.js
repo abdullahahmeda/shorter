8d73d71b39a3aad924f860faf9f039e49fb23d4c
Abdullah Ahmed <abdullah.ahmed.a2000@gmail.com>
2020-01-09 15:44:32 +0200

    Updates

---
+++ precache-manifest.3d89756925fe67e71c7d0625caa0b05c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6a39fac5fe3166a22a0abfba05416d",
+    "url": "/shorter/index.html"
+  },
+  {
+    "revision": "86ef0970fc9880bd21fb",
+    "url": "/shorter/static/css/main.ede7f12d.chunk.css"
+  },
+  {
+    "revision": "7abf67c0c87bf324f7de",
+    "url": "/shorter/static/js/2.f561441e.chunk.js"
+  },
+  {
+    "revision": "eaa93899759cd78ff2a4147c0af30aa2",
+    "url": "/shorter/static/js/2.f561441e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86ef0970fc9880bd21fb",
+    "url": "/shorter/static/js/main.0dc024dd.chunk.js"
+  },
+  {
+    "revision": "ef6fd24e00043351449d",
+    "url": "/shorter/static/js/runtime-main.88cc86f9.js"
+  }
+]);
